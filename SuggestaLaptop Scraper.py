c1221691f5bb50bd2dc8bbc3b8deec92438a6deb
072665995 <chau413@gmail.com>
2015-09-26 00:27:09 -0400

    Added notes
    Added important notes that need to be read before running. Added
    comments to make reading the code easier

--- SuggestaLaptop Scraper.py
+++ SuggestaLaptop Scraper.py
@@ -8,6 +8,8 @@
 # Created:		06/07/2015
 # ------------------------------------------------------------------------------
 
+# IMPORTANT: outdated since Reddit mandated OAUTH2.0 
+
 import praw
 import time
 
@@ -16,6 +18,7 @@ r = praw.Reddit("SuggestaLaptop bot scraper /u/072665995")
 r.login("REDDITAPIBRO", "biostarz77ke")
 
 has_key = ("$1000", "CAN", "Canada", "Engineering", "University")
+# Used to keep track of what has already been looked at so bot does not do double work
 scanned = []
 
 while True:
@@ -23,6 +26,7 @@ while True:
     for submission in subreddit.get_hot(limit=10):
         original_text = submission.selftext
         has_laptop = any(string in original_text for string in has_key)
+		# Test if the words in original_text contain any of the keywords
         if submission.id not in scanned and has_key:
             message = "Read this thread" % submission.short_link
             r.send_message("072665995", "Laptop Search Find", message)
